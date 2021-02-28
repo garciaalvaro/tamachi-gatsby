305c426815989306ded8495938f3279e1a261ed1
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-02-28 18:05:23 +0100

    Included center prop in Container

--- src/components/Single/Container/Container.tsx
+++ src/components/Single/Container/Container.tsx
@@ -7,16 +7,18 @@ interface Props {
 	use_border?: boolean;
 	full_width?: boolean;
 	max_width?: number;
+	center?: number;
 }
 
 export const Container: FunctionComponent<Props> = props => {
-	const { use_border, full_width, max_width } = props;
+	const { center, use_border, full_width, max_width } = props;
 
 	return (
 		<div
 			className={className([
 				full_width ? styles.full_width : null,
 				use_border ? styles.use_border : null,
+				center ? styles.center : null,
 			])}
 			style={{ maxWidth: max_width }}
 		>
